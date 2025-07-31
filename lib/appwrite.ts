a3e1d93cc35c50ed527ccde061d280293e6a5605
Cherillkin <108462023+Cherillkin@users.noreply.github.com>
2025-07-31 22:26:48 +0600

    Create authentification

---
+++ lib/appwrite.ts
@@ -0,0 +1,7 @@
+export const appwriteConfig = {
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  platform: "com.klimen.food",
+  databaseId: "688a323a00251388cb7c",
+  userCollection: "688a327a00298e4f570b",
+};
